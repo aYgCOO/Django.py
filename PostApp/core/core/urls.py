42fe5ea5ce7c4e52af6f4fb4e91a9080f19c2730
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-04-23 00:37:15 +0530

    Updated

--- PostApp/core/core/urls.py
+++ PostApp/core/core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mygallary.views import home, form
+from mygallary.views import home, form, delete, edit
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('new-post/', form, name='form')
+    path('new-post/', form, name='form'),
+    path('delete/<int:id>/', delete, name='delete'),
+    path('edit-post/<int:id>/', edit, name='edit-post')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- PostApp/core/mygallary/views.py
+++ PostApp/core/mygallary/views.py
@@ -1,14 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from mygallary.models import newpost
 from django.contrib import messages
 
 # Create your views here.
+
+#Home 
 def home(request):
      db = newpost.objects.all().order_by('-date')
      context = {'db': db}
-    #  return render(request, 'home/index.html', context=context)
      return render(request, 'home/index.html',context)
 
+
+#New post
 def form(request):
     if request.method == 'POST':
         caption = request.POST.get('caption')
@@ -17,8 +20,36 @@ def form(request):
         np.save()
         db = newpost.objects.all()
         messages.success(request, "Your post is uploaded successfully")
-       
-        
         return redirect('/')  
     else:
         return render(request, 'home/form.html')
+    
+
+#Delete post
+def delete(request, id):
+    db = newpost.objects.get(id=id)
+    db.delete()
+    messages.success(request, "Your post is deleted successfully")
+    return redirect('/')
+
+
+#Edit post
+def edit(request, id):
+    post = get_object_or_404(newpost, id=id)
+    
+    if request.method == 'POST':
+        edit_caption = request.POST.get('edit-caption')
+        edit_image = request.FILES.get('edit-image')
+
+        if edit_caption or edit_image:
+            post.edit_caption = edit_caption
+            if edit_image:
+                post.edit_image = edit_image
+            post.save()
+            return redirect('home')
+    else:
+        context = {'post': post, 'error': 'Oops! Something went wrong :('}
+        return render(request, 'home/edit.html', context=context)
+    
+    
+
